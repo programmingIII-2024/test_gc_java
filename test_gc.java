2ef88d9834c64c6398a6167b695de796f745c864
ohba <ohba@sendai-nct.ac.jp>
2025-02-12 18:44:28 +0900

    不要メッセージを削除

--- test_gc.java
+++ test_gc.java
@@ -9,14 +9,13 @@ public class test_gc {
 		{}
 
 		System.out.printf("Start\n");
-        long start = System.currentTimeMillis();  // 開始時刻を取得
+
         String s = "abc";
         for(int i=0; i<1000; i++) {
             for(int j=0; j<250; j++) {
                 s += "a";  //文字列の結合
             }
         }
-        long finish = System.currentTimeMillis(); // 終了時刻の取得
-        System.out.printf("実行時間：%f秒\n",(finish-start)/1000.0);  //処理にかかった時間の表示
+        System.out.printf("Finish\n"); 
     }
 }
